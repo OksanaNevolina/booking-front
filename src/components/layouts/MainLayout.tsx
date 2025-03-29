be2e3a992a98f025e146b6e62074560cb12fd704
OksanaNevolina <onevolina5gmail.com>
2025-03-29 19:26:46 +0200

    added png

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import {Header} from "../Header/Header";
+import css from "./MainLayout.module.css"
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className={css.mainBox}>
             <Header/>
             <Outlet />
         </div>
